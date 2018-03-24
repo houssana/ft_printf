4d8da0576f8f6ea151286894dcb2b73c2e9ac3f0
Hamid OUSSANA <houssana@e3r8p21.42.fr>
2018-03-24 17:03:27 +0100

    bye42

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "libft/libft.h"
+
 void foo(char *fmt, ...);
 
 int ft_printf(const char * restrict format, ...)
@@ -7,6 +9,10 @@ int ft_printf(const char * restrict format, ...)
 	{
 		if (*format != '%')	
 			ft_putchar(*format);
+		else if (format[1] == '%')
+			ft_putchar(*format++);
+		else
+			ft_putnbr(ft_atoi(&format[1]));
 		format++;
 	}
 }
@@ -14,6 +20,6 @@ int ft_printf(const char * restrict format, ...)
 int main(int argc, char **argv)
 {
 	foo("d s", 42, "comment ca va ?");
-	printf("%d\n", printf("%2$0-5s%%d\n", "12345678", "9"));
-	ft_printf("bonjour %s\n");
+	printf("%d\n", printf("%3$ 05d%%d\n", "12345678", "9", 4));
+	ft_printf("bonjour %\n2.4s%%%\n");
 }
